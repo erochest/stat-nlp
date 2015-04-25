25a2bf33c1c7e3bd42de51eb2ea29338a5ffe647
Eric Rochester <erochest@virginia.edu>
2015-04-24 22:59:40 -0400

    Output top collocates.

--- Main.hs
+++ Main.hs
@@ -5,10 +5,14 @@ module Main where
 
 
 import           Conduit
-import qualified Data.Conduit.Text as CT
-import qualified Data.Text         as T
-import qualified Data.Text.Format  as F
-import           Taygeta.Tokenizer (regexTokenizer)
+import           Control.Monad.Identity
+import           Data.Bifunctor
+import qualified Data.Conduit.Text      as CT
+import qualified Data.HashMap.Strict    as M
+import qualified Data.Text              as T
+import qualified Data.Text.Format       as F
+import           Data.Traversable
+import           Taygeta.Tokenizer      (regexTokenizer)
 
 import           StatNLP.Output
 import           StatNLP.Types
@@ -17,7 +21,6 @@ import           StatNLP.Utils
 {-
  - **TODO**: Research Wittgenstein's *use theory of meaning*.
  -
- - TODO: collocate generator
  - TODO: n-gram generator
  - TODO: stopword filter
  - TODO: inverse index
@@ -27,10 +30,20 @@ import           StatNLP.Utils
 
 main :: IO ()
 main = do
-    freqs <- runResourceT $  stdinC
-                          $= CT.linesBounded (2^30)
-                          $= concatMapC (regexTokenizer "[\\p{L}\\p{M}]+")
-                          $= mapC T.toLower
-                          $$ frequenciesC
+    -- tokens
+    tokens <- runResourceT $  stdinC
+                           $= CT.linesBounded (2^30)
+                           $= concatMapC (regexTokenizer "[\\p{L}\\p{M}]+")
+                           $= mapC T.toLower
+                           $$ sinkList
+
+    -- frequencies
+    let freqs = frequencies tokens
+    putStrLn "Frequencies"
     freqReport 25 freqs
-    F.print "Token/type ratio = {}\n" . F.Only $ tokenTypeRatio freqs
+    F.print "Token/type ratio = {}\n\n" . F.Only $ tokenTypeRatio freqs
+
+    -- collocates
+    let cols = frequencies $ collocates 3 3 tokens
+    putStrLn "Collocates"
+    freqReport 25 . M.fromList . fmap (first show) $ M.toList cols
--- src/StatNLP/Output.hs
+++ src/StatNLP/Output.hs
@@ -7,12 +7,13 @@ module StatNLP.Output where
 import qualified Data.HashMap.Strict as M
 import qualified Data.List           as L
 import           Data.Ord
+import           Data.Text.Buildable
 import qualified Data.Text.Format    as F
 
 import           StatNLP.Types
 
 
-freqReport :: Int -> FreqMap PlainToken -> IO ()
+freqReport :: Buildable ps => Int -> FreqMap ps -> IO ()
 freqReport n = mapM_ (F.print "{}\t{}\n")
              . L.take n
              . L.sortBy (comparing (Down . snd))
