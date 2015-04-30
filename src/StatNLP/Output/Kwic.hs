8540ec551abc4f3fde4cb4899c2ba7258696f67a
Eric Rochester <erochest@virginia.edu>
2015-04-30 10:38:23 -0400

    Remove missing KwicNode.

--- src/StatNLP/Output/Kwic.hs
+++ src/StatNLP/Output/Kwic.hs
@@ -4,11 +4,11 @@ module StatNLP.Output.Kwic where
 import StatNLP.Types
 
 
-kwic :: PlainToken -> Index PlainToken SpanPos -> Int -> Document -> [KwicNode]
+kwic :: PlainToken -> Index PlainToken SpanPos -> Int -> Document -> ()
 kwic = undefined
 
-posKwic :: PlainToken -> SpanPos -> Int -> Document -> [KwicNode]
+posKwic :: PlainToken -> SpanPos -> Int -> Document -> ()
 posKwic = undefined
 
-concordance :: Index PlainToken SpanPos -> Int -> Document -> [(PlainToken, [KwicNode])]
+concordance :: Index PlainToken SpanPos -> Int -> Document -> [(PlainToken, ())]
 concordance = undefined
