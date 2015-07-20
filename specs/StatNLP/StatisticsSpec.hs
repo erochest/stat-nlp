a6b146f73453fcb6b77e318693408591874eb00a
Eric Rochester <erochest@virginia.edu>
2015-07-20 07:14:25 -0400

    Precision.

--- specs/StatNLP/StatisticsSpec.hs
+++ specs/StatNLP/StatisticsSpec.hs
@@ -35,11 +35,12 @@ spec = do
                        . swap
                        . fmap (0,)
                        $ sgt (MHash $ Sum <$> input) 1.96
-                m      = 1.0e-8
+                m      = 1.0e-4
             M.keys actual `shouldMatchList` M.keys expected
             forM_ (L.sort $ M.keys actual) $ \k ->
                 let a = M.lookupDefault 0 k actual
                     e = M.lookupDefault 0 k expected
                 in  assertBool (  "WRONG " ++ show k ++ ": "
-                               ++ show a ++ " != " ++ show e)
+                               ++ show a ++ " != " ++ show e
+                               ++ " (" ++ show (abs (a - e)) ++ ")")
                                $ abs (a - e) <= m
